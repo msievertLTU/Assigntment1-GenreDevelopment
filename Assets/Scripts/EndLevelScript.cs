d273ff4a5529507830c58d172e5122a411af79ac
msievertLTU <60272260+msievertLTU@users.noreply.github.com>
2020-01-27 20:52:29 -0500

    Added Victyor/Level Completed Scene
    Added a scene to show level has been completed with buttons to exit or return to menu.

---
+++ Assets/Scripts/EndLevelScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelScript : MonoBehaviour
+{
+    //public GameObject player;
+    //public GameObject flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //flag = GetComponent<GameObject>();
+        
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Victory");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
